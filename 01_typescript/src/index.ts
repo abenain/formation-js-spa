6e47d8d699be66126820d2320f69f98eeed96464
Antoine Benain <antoine@zzlaptop.local>
2016-12-26 20:25:25 +0100

    Switch to Typescript -> see compilation error

---
+++ 01_typescript/src/index.ts
@@ -0,0 +1,32 @@
+/**
+ * Created by antoine on 22/12/2016.
+ */
+
+import * as numeral from 'numeral';
+import * as jquery from 'jquery';
+
+var formatPrice = function(price){
+	var format = '$0,0.00',
+		number = numeral(price.trim())
+
+	return number.format(format)
+}
+
+window.onload = function () {
+	jquery.get('http://localhost:3000/books').done(function(result){
+		var tableContents = ''
+        
+		for(var i=0; i<result.length; i++){
+            tableContents += '<tr>'
+            tableContents += '<td>' + result[i].title + '</td>'
+            tableContents += '<td>' + result[i].author + '</td>'
+            tableContents += '<td>' + result[i].genre + '</td>'
+            tableContents += '<td>' + result[i].publish_date + '</td>'
+            tableContents += '<td>' + formatPrice(result[i].price) + '</td>'
+            tableContents += '</tr>'
+		}
+
+		document.getElementById('table-body').innerHTML = tableContents
+	})
+}
+
--- 01_typescript/webpack.config.js
+++ 01_typescript/webpack.config.js
@@ -2,10 +2,15 @@
  * Created by antoine on 22/12/2016.
  */
 module.exports = {
-	entry: './src/index.js',
+	entry: './src/index.ts',
 	output: {
 		path: './build',
 		publicPath: '/build/',
 		filename: 'app.bundle.js'
+	},
+	module: {
+		loaders: [
+			{ test: /\.ts(x?)$/, exclude: /node_modules/, loader: 'babel-loader!ts-loader' }
+		]
 	}
 };
