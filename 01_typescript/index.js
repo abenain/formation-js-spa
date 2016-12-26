0b86564cac8280ab6610cfdce716e07f29b42702
Antoine Benain <antoine@zzlaptop.local>
2016-12-26 19:32:38 +0100

    Basic project structure

---
+++ 01_typescript/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by antoine on 22/12/2016.
+ */
+window.handleClick = function () {
+	alert('You clicked the button!')
+}
---
+++ 01_typescript/webpack.config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by antoine on 22/12/2016.
+ */
+module.exports = {
+	entry: './index.js',
+	output: {
+		path: './build',
+		filename: 'app.bundle.js'
+	}
+};
