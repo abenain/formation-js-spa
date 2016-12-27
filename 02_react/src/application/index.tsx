dc0a62db0deafce0c2afc6d757b5254c7c8e4f7d
Antoine Benain <antoine@zzlaptop.local>
2016-12-27 02:34:29 +0100

    Add title to Header props

--- 02_react/src/application/index.tsx
+++ 02_react/src/application/index.tsx
@@ -5,8 +5,40 @@ import * as React from 'react'
 
 import Header from "header"
 
+const title = 'Test ReactJS Application'
+
+/*const books = [{
+ "id": "bk001",
+ "title": "Everyday Italian",
+ "author": "Giada De Laurentiis",
+ "genre": "Languages",
+ "publish_date": "2005",
+ "price": 30
+ }, {
+ "id": "bk002",
+ "title": "Harry Potter",
+ "author": "J K. Rowling",
+ "genre": "Fantasy",
+ "publish_date": "2005",
+ "price": 29.99
+ }, {
+ "id": "bk003",
+ "title": "XQuery Kick Start",
+ "author": "James McGovern",
+ "genre": "Computer",
+ "publish_date": "2003",
+ "price": 49.99
+ }, {
+ "id": "bk004",
+ "title": "Learning XML",
+ "genre": "Computer",
+ "author": "Erik T. Ray",
+ "publish_date": "2003",
+ "price": 39.95
+ }]*/
+
 const Application = () => <div>
-    <Header />
+    <Header title={title}/>
     <h1>Hello, World!</h1>
 </div>
 
--- 02_react/src/header/index.tsx
+++ 02_react/src/header/index.tsx
@@ -4,10 +4,13 @@
 import * as React from 'react'
 
 const styles = require('./styles.scss')
-const title = 'Test ReactJS Application'
 const logo = require('./logo.svg')
 
-const Header = () => <div className={styles.container}>
+interface Props{
+    title: string
+}
+
+const Header = ({title}: Props) => <div className={styles.container}>
     <img src={logo} className={styles.logo} alt='React logo'/>
     <div className={styles.title}>{title}</div>
 </div>
