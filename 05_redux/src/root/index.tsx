484d4d67b8846ed307bbd2376f345972a6f9bd4f
Antoine Benain <antoine@zzlaptop.local>
2016-12-27 11:22:55 +0100

    Chapter 05: provide redux store to route components

--- 05_redux/src/root/index.tsx
+++ 05_redux/src/root/index.tsx
@@ -8,19 +8,23 @@ import Route from 'react-router/lib/Route'
 import IndexRoute from 'react-router/lib/IndexRoute'
 import browserHistory from 'react-router/lib/browserHistory'
 import injectTapEventPlugin from 'react-tap-event-plugin';
-
+import { createStore } from 'redux'
 
 import Application from 'application'
 import Books from "books"
 import Home from 'home'
+import reducer from "./reducer"
 
 require('./styles.scss')
+
+const store = createStore(reducer)
 
 injectTapEventPlugin();
 
 window.onload = () => {
     ReactDOM.render(
-        <Router history={browserHistory}>
+        <Router history={browserHistory}
+                createElement={(component: React.ComponentClass<any>, props: any) => React.createElement(component, Object.assign({}, props, { store }))}>
             <Route path='/' component={Application}>
                 <Route path='/books' component={Books} />
                 <IndexRoute component={Home}/>
---
+++ 05_redux/src/root/reducer.ts
@@ -0,0 +1,5 @@
+const reducer = (state = {}, action: any) => {
+    return {}
+}
+
+export default reducer
