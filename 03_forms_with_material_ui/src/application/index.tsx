9c0607072dfd2a791f77c5d48ad47024a4090786
Antoine Benain <antoine@zzlaptop.local>
2016-12-27 04:22:23 +0100

    Chapter 03: Initial commit

---
+++ 03_forms_with_material_ui/src/application/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+import Header from 'header'
+
+const title = 'Test ReactJS Application'
+
+const Application = () => <div>
+    <Header title={title}></Header>
+    <h1>Hello, World!</h1>
+</div>
+
+export default Application
