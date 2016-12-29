8d10b63ecda621ff2a968626cbdbbc048ce955c7
Antoine Benain <antoine@zzlaptop.local>
2016-12-29 17:06:55 +0100

    Chapter 01: Classes no fat arrow => this error

--- 01_typescript/src/index.ts
+++ 01_typescript/src/index.ts
@@ -13,39 +13,33 @@ interface Book {
     price:number
 }
 
-const makeTable = function (books:Book[]) {
-    const formatPrice = function (price:number) {
+class Table{
+    constructor(private books: Book[]){}
+
+    static formatPrice = function(price:number) {
         var format = '$0,0.00',
             number = numeral(price)
 
         return number.format(format)
     }
 
-    return {
-        books: books,
-        render: function () {
-            return this.books.map(function (book:Book) {
-                return '<tr>'
-                    + '<td>' + book.title + '</td>'
-                    + '<td>' + book.author + '</td>'
-                    + '<td>' + book.genre + '</td>'
-                    + '<td>' + book.publish_date + '</td>'
-                    + '<td>' + formatPrice(book.price) + '</td>'
-                    + '</tr>'
-            }).join('')
-        }
+    public render() {
+        return this.books.map(function (book:Book) {
+            return '<tr>'
+                + '<td>' + book.title + '</td>'
+                + '<td>' + book.author + '</td>'
+                + '<td>' + book.genre + '</td>'
+                + '<td>' + book.publish_date + '</td>'
+                + '<td>' + Table.formatPrice(book.price) + '</td>'
+                + '</tr>'
+        }).join('')
+
     }
 }
 
-
 window.onload = function () {
-    let table = makeTable([])
-
-    // The following line will cause click handler to fail beacuse this is not what expected
-    $('button.ff-render-table-button').click(table.render) // ERROR: Uncaught TypeError: Cannot read property 'map' of undefined
-
-    $.get('http://localhost:3000/books').done(function (result) {
-        table = makeTable(result)
+    $.get('http://codeberry.fr/1/books').done(function (result) {
+        const table = new Table(result)
 
         $('#table-body').html(table.render())
     })
