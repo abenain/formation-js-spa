c802c9ebe503058ee32d9bec1b63b3609e163a58
Antoine Benain <antoine@zzlaptop.local>
2016-12-29 17:14:11 +0100

    Chapter 01: Classes with fat arrow => this error fixed

--- 01_typescript/src/index.ts
+++ 01_typescript/src/index.ts
@@ -13,35 +13,32 @@ interface Book {
     price:number
 }
 
-class Table{
-    constructor(private books: Book[]){}
+class Table {
+    constructor(private books:Book[]) {
+    }
 
-    static formatPrice = function(price:number) {
+    static formatPrice = function (price:number) {
         var format = '$0,0.00',
             number = numeral(price)
 
         return number.format(format)
     }
 
-    public render() {
-        return this.books.map(function (book:Book) {
-            return '<tr>'
-                + '<td>' + book.title + '</td>'
-                + '<td>' + book.author + '</td>'
-                + '<td>' + book.genre + '</td>'
-                + '<td>' + book.publish_date + '</td>'
-                + '<td>' + Table.formatPrice(book.price) + '</td>'
-                + '</tr>'
-        }).join('')
-
-    }
+    public render = () => this.books.map((book:Book) => {
+        return '<tr>'
+            + '<td>' + book.title + '</td>'
+            + '<td>' + book.author + '</td>'
+            + '<td>' + book.genre + '</td>'
+            + '<td>' + book.publish_date + '</td>'
+            + '<td>' + Table.formatPrice(book.price) + '</td>'
+            + '</tr>'
+    }).join('')
 }
 
 window.onload = function () {
     let table = new Table([])
 
-    // The following line will cause click handler to fail beacuse this is not what expected
-    $('button.ff-render-table-button').click(table.render) // ERROR: Uncaught TypeError: Cannot read property 'map' of undefined
+    $('button.ff-render-table-button').click(table.render)
 
     $.get('http://localhost:3000/books').done(function (result) {
         const table = new Table(result)
