0e444be3797ca8d79cdc479d0bea28bf75d05cc2
Antoine Benain <antoine@zzlaptop.local>
2016-12-29 16:34:32 +0100

    Chapter 01: Create table component functional => WIHOUT FAT arrow

--- 01_typescript/src/index.ts
+++ 01_typescript/src/index.ts
@@ -3,7 +3,7 @@
  */
 
 import * as numeral from 'numeral';
-import * as jquery from 'jquery';
+import * as $ from 'jquery';
 
 interface Book {
     title:string
@@ -13,28 +13,41 @@ interface Book {
     price:number
 }
 
-var formatPrice = function (price:number) {
-    var format = '$0,0.00',
-        number = numeral(price)
+const makeTable = function (books:Book[]) {
+    const formatPrice = function (price:number) {
+        var format = '$0,0.00',
+            number = numeral(price)
 
-    return number.format(format)
+        return number.format(format)
+    }
+
+    return {
+        books: books,
+        render: function () {
+            return this.books.map(function (book:Book) {
+                return '<tr>'
+                    + '<td>' + book.title + '</td>'
+                    + '<td>' + book.author + '</td>'
+                    + '<td>' + book.genre + '</td>'
+                    + '<td>' + book.publish_date + '</td>'
+                    + '<td>' + formatPrice(book.price) + '</td>'
+                    + '</tr>'
+            }).join('')
+        }
+    }
 }
 
+
 window.onload = function () {
-    jquery.get('http://localhost:3000/books').done(function (result) {
-        var tableContents = ''
+    let table = makeTable([])
 
-        result.forEach((book:Book) => {
-            tableContents += '<tr>'
-            tableContents += '<td>' + book.title + '</td>'
-            tableContents += '<td>' + book.author + '</td>'
-            tableContents += '<td>' + book.genre + '</td>'
-            tableContents += '<td>' + book.publish_date + '</td>'
-            tableContents += '<td>' + formatPrice(book.price) + '</td>'
-            tableContents += '</tr>'
-        })
+    // The following line will cause click handler to fail beacuse this is not what expected
+    $('button.ff-render-table-button').click(table.render) // ERROR: Uncaught TypeError: Cannot read property 'map' of undefined
 
-        document.getElementById('table-body').innerHTML = tableContents
+    $.get('http://localhost:3000/books').done(function (result) {
+        table = makeTable(result)
+
+        $('#table-body').html(table.render())
     })
 }
 
