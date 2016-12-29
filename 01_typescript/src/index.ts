e0aacd5647c79bf7379122c9930e8316bba88745
Antoine Benain <antoine@zzlaptop.local>
2016-12-29 17:23:22 +0100

    Chapter 01: Create table component functional

--- 01_typescript/src/index.ts
+++ 01_typescript/src/index.ts
@@ -13,35 +13,40 @@ interface Book {
     price:number
 }
 
-class Table {
-    constructor(private books:Book[]) {
-    }
+const makeTable = (bookList?: Book[]) => {
+    const books = bookList || []
 
-    static formatPrice = function (price:number) {
+    const formatPrice = function (price:number) {
         var format = '$0,0.00',
             number = numeral(price)
 
         return number.format(format)
     }
 
-    public render = () => this.books.map((book:Book) => {
-        return '<tr>'
-            + '<td>' + book.title + '</td>'
-            + '<td>' + book.author + '</td>'
-            + '<td>' + book.genre + '</td>'
-            + '<td>' + book.publish_date + '</td>'
-            + '<td>' + Table.formatPrice(book.price) + '</td>'
-            + '</tr>'
-    }).join('')
+    return {
+        updateBooks: (newBooks: Book[]) => {
+            books.splice(0, books.length)
+            books.push(...newBooks)
+        },
+        render: () => books.map((book:Book) => {
+            return '<tr>'
+                + '<td>' + book.title + '</td>'
+                + '<td>' + book.author + '</td>'
+                + '<td>' + book.genre + '</td>'
+                + '<td>' + book.publish_date + '</td>'
+                + '<td>' + formatPrice(book.price) + '</td>'
+                + '</tr>'
+        }).join('')
+    }
 }
 
 window.onload = function () {
-    let table = new Table([])
+    const table = makeTable([])
 
     $('button.ff-render-table-button').click(table.render)
 
     $.get('http://localhost:3000/books').done(function (result) {
-        const table = new Table(result)
+        table.updateBooks(result)
 
         $('#table-body').html(table.render())
     })
