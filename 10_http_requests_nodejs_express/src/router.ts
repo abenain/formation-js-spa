8610487ecba37199b8d1a6ed8bf2ccd08254b924
Antoine Benain <antoine@zzlaptop.local>
2017-01-15 22:22:57 +0100

    Chapter 10: read books from file

--- 10_http_requests_nodejs_express/src/router.ts
+++ 10_http_requests_nodejs_express/src/router.ts
@@ -1,12 +1,18 @@
 /**
  * Created by antoine on 15/01/2017.
  */
-import { Application } from 'express'
+import * as Express from 'express'
 
 import { createRouter as createBooksRouter } from './api/books'
 
+const setDefaultHeaders = (request: Express.Request, response: Express.Response, next: Express.NextFunction) => {
+    response.header('Access-Control-Allow-Origin', '*')
+    next()
+}
+
 const Router = {
-    createRoutes: (app: Application) => {
+    createRoutes: (app: Express.Application) => {
+        app.use(setDefaultHeaders)
         app.use(createBooksRouter())
     }
 }
