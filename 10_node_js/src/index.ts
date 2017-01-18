32cb15be008f9352abf850285bbcf6001f361b7a
Antoine Benain <antoine.benain@gmail.com>
2017-01-18 20:44:39 +0100

    Chapter 10: read all files in data directory using promises

--- 10_node_js/src/index.ts
+++ 10_node_js/src/index.ts
@@ -3,34 +3,35 @@
  */
 import * as fs from 'fs'
 import * as path from 'path'
+import * as q from 'q'
+
+
+const readFile = q.denodeify(fs.readFile)
+const readDir = q.denodeify(fs.readdir)
 
 // Construct path to data directory
 const dirPath = path.join(__dirname, '..', 'data')
 
 // async reading of data directory contents
-fs.readdir(dirPath, (error, dirContents) => {
-
-    // Deal with errors
-    if(Boolean(error)){
-        throw error
-    }
+readDir(dirPath).then((dirContents: string[]) => {
 
     // Looping on filenames
-    dirContents.forEach((filename) => {
+    dirContents.forEach((filename: string) => {
 
         // Construct path to file
         const filePath = path.join(dirPath, filename)
 
         // async reading of file contents
-        fs.readFile(filePath, (error, data) => {
-
-            // Deal with errors
-            if(Boolean(error)){
-                throw error
-            }
-
+        readFile(filePath).then((data: number) => {
+            // Printing file contents
             console.log('Contents of file ' + filePath)
             console.log(JSON.parse(data.toString()))
+        }).catch((error) => {
+            // Deal with errors
+            throw error
         })
     })
+}).catch((error) => {
+    // Deal with errors
+    throw error
 })
