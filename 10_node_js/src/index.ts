d796c896ef63ee38107563710b9edf5cbb21c80e
Antoine Benain <antoine.benain@gmail.com>
2017-01-18 20:15:45 +0100

    Chapter 10: read directory contents

--- 10_node_js/src/index.ts
+++ 10_node_js/src/index.ts
@@ -1,4 +1,16 @@
 /**
  * Created by antoine on 22/12/2016.
  */
-console.log('Hello, World!')
+import * as fs from 'fs'
+import * as path from 'path'
+
+const dirPath = path.join(__dirname, '..', 'data')
+
+fs.readdir(dirPath, (error, data) => {
+    if(Boolean(error)){
+        throw error
+    }
+
+    console.log('Contents of directory ' + dirPath)
+    console.log(data)
+})
