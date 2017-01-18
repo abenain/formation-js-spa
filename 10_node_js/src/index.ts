e6466cef87cf5dcc9bc9b39310fe01a6e900a21f
Antoine Benain <antoine.benain@gmail.com>
2017-01-18 21:30:28 +0100

    Chapter 10: read all files with q.all

--- 10_node_js/src/index.ts
+++ 10_node_js/src/index.ts
@@ -12,8 +12,16 @@ const readDir = q.denodeify(fs.readdir)
 // Construct path to data directory
 const dirPath = path.join(__dirname, '..', 'data')
 
+interface File {
+    path: string
+    contents: number
+}
+
 // async reading of data directory contents
 readDir(dirPath).then((dirContents: string[]) => {
+
+    // Initializing array of promises
+    const promises: q.Promise<File>[] = []
 
     // Looping on filenames
     dirContents.forEach((filename: string) => {
@@ -21,17 +29,36 @@ readDir(dirPath).then((dirContents: string[]) => {
         // Construct path to file
         const filePath = path.join(dirPath, filename)
 
-        // async reading of file contents
-        readFile(filePath).then((data: number) => {
-            // Printing file contents
-            console.log('Contents of file ' + filePath)
-            console.log(JSON.parse(data.toString()))
-        }).catch((error) => {
-            // Deal with errors
-            throw error
-        })
+        // adding readFile promise to the promise array
+        promises.push(readFile(filePath).then((fileContents: number) => ({
+            path: filePath,
+            contents: fileContents
+        })))
     })
+
+    // return a promise that'll resolve when all promises in array have resolved
+    return q.all(promises)
+
+}).then((allFiles: File[]) => {
+
+    console.log('--- Contents of directory ' + dirPath)
+    
+    // loop through array of results from readFile promises
+    allFiles.forEach(file => {
+
+        // Parsing file contents to a javascript object
+        const fileContents = JSON.parse(file.contents.toString())
+
+        // Printing file contents
+        console.log('[' + fileContents.id + '] -> ' + file.path)
+    })
+    
+    console.log('---')
+    console.log(allFiles.length + ' items found')
+
 }).catch((error) => {
+
     // Deal with errors
     throw error
+
 })
