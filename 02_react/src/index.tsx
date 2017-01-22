b585bc54352798eae2851113412a7406bed46dab
Antoine Benain <antoine.benain@gmail.com>
2017-01-22 21:48:45 +0100

    Chapter 02: Creating a Clock component as class

--- 02_react/src/index.tsx
+++ 02_react/src/index.tsx
@@ -4,16 +4,20 @@
 import * as ReactDOM from 'react-dom'
 import * as React from 'react'
 
-const rerender = () => {
-    const element = (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    )
+class Clock extends React.Component<{}, {}> {
+    public render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            </div>
+        )
+    }
+}
 
+const rerender = () => {
     ReactDOM.render(
-        element,
+        <Clock />,
         document.getElementById('root')
     )
 }
