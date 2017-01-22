79529e44dae837f7746c87a58a5f2cf3c9c52a11
Antoine Benain <antoine.benain@gmail.com>
2017-01-22 21:49:21 +0100

    Chapter 02: Creating a Clock functional component

--- 02_react/src/index.tsx
+++ 02_react/src/index.tsx
@@ -4,16 +4,12 @@
 import * as ReactDOM from 'react-dom'
 import * as React from 'react'
 
-class Clock extends React.Component<{}, {}> {
-    public render() {
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {new Date().toLocaleTimeString()}.</h2>
-            </div>
-        )
-    }
-}
+const Clock = () => (
+    <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+)
 
 const rerender = () => {
     ReactDOM.render(
