8751df08d4ace9e75b52954ff2bd95b293bfabf8
Antoine Benain <antoine.benain@gmail.com>
2017-01-22 23:28:37 +0100

    Chapter 02: lifecycle hooks

--- 02_react/src/index.tsx
+++ 02_react/src/index.tsx
@@ -4,20 +4,45 @@
 import * as ReactDOM from 'react-dom'
 import * as React from 'react'
 
-interface Props {
+interface State {
     date: Date
 }
 
-const Clock = (props: Props) => (
-    <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-)
+class Clock extends React.Component<{}, State> {
+    public constructor(props: {}){
+        super(props)
+        this.state = {
+            date: new Date()
+        }
+    }
+    private timerID: number
+    public componentDidMount = () => {
+        this.timerID = setInterval(
+            () => this.updateTime(),
+            1000
+        );
+    }
+    public componentWillUnmount = () => {
+        clearInterval(this.timerID)
+    }
+    private updateTime = () => {
+        this.setState({
+            date: new Date()
+        })
+    }
+    public render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        )
+    }
+}
 
 const rerender = () => {
     ReactDOM.render(
-        <Clock date={new Date()} />,
+        <Clock />,
         document.getElementById('root')
     )
 }
