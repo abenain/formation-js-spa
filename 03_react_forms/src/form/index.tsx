798ca4ee146742284e0cf40df12241425fae7c6b
Antoine Benain <antoine.benain@gmail.com>
2017-01-23 20:30:39 +0100

    Chapter 03: controlled inputs exercise SOLUTION

--- 03_react_forms/src/form/index.tsx
+++ 03_react_forms/src/form/index.tsx
@@ -6,7 +6,10 @@ import * as React from 'react'
 const styles = require('./styles.scss')
 
 interface State{
-    name: string
+    title: string,
+    reference: string,
+    object: string,
+    nature: number
 }
 
 class Form extends React.Component<{}, State>{
@@ -19,28 +22,90 @@ class Form extends React.Component<{}, State>{
     public constructor(props: {}){
         super(props)
         this.state = {
-            name: ''
+            title: '',
+            reference: '',
+            object: '',
+            nature: 1
         }
+    }
+
+    private onValueChange = (key: string, event: React.FormEvent<any>) => {
+        const newValue = (event.target as any).value
+        const newState: {[k: string]: any} = {}
+        newState[key] = newValue as any
+        this.setState(newState as State)
     }
     
     private handleFormSubmit = (event: React.SyntheticEvent<any>) => {
+        const documentValues = {
+            title: this.state.title,
+            reference: this.state.reference,
+            object: this.state.object,
+            nature: this.state.nature
+        }
         console.log('Form was submitted')
-        console.log('New name is: ' + this.refs.name.value)
+        console.log('Document values: ' + JSON.stringify(documentValues))
     }
 
     public render(){
         return (
             <div className={styles.container}>
-                <label>
-                    Name:
-                    <input className={styles.input}
-                           type="text"
-                           name="name"
-                           ref="name"
-                           defaultValue='Enter your name' />
-                </label>
-                <button className={styles.button}
-                        onClick={this.handleFormSubmit}>Save</button>
+                <div className={styles.row}>
+                    <div className={styles.labelColumn}>
+                        <span>Titre du document:</span>
+                    </div>
+                    <div className={styles.inputColumn}>
+                        <input className={styles.input}
+                               type="text"
+                               name="title"
+                               value={this.state.title}
+                               onChange={(event: React.FormEvent<any>) => this.onValueChange('title', event)} />
+                    </div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.labelColumn}>
+                        <span>Référence du document:</span>
+                    </div>
+                    <div className={styles.inputColumn}>
+                        <input className={styles.input}
+                               type="text"
+                               name="reference"
+                               value={this.state.reference}
+                               onChange={(event: React.FormEvent<any>) => this.onValueChange('reference', event)} />
+                    </div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.labelColumn}>
+                        <span>Objet du document:</span>
+                    </div>
+                    <div className={styles.inputColumn}>
+                         <textarea className={styles.textarea}
+                                   rows={4}
+                                   name="object"
+                                   value={this.state.object}
+                                   onChange={(event: React.FormEvent<any>) => this.onValueChange('object', event)} />
+                    </div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.labelColumn}>
+                        <span>Nature du document:</span>
+                    </div>
+                    <div className={styles.inputColumn}>
+                        <select className={styles.select}
+                                name="nature"
+                                value={this.state.nature}
+                                onChange={(event: React.FormEvent<any>) => this.onValueChange('nature', event)} >
+                            <option value="1">Mémo</option>
+                            <option value="2">Note</option>
+                            <option value="3">Procédure</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className={styles.buttonPanel}>
+                    <button className={styles.button}
+                            onClick={this.handleFormSubmit}>Save</button>
+                </div>
             </div>
         )
     }
