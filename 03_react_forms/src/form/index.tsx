d2389adc24f03584cb9a6b0bfdd5c130530b9ede
Antoine Benain <antoine.benain@gmail.com>
2017-01-23 19:15:41 +0100

    Chapter 03: controlled input

--- 03_react_forms/src/form/index.tsx
+++ 03_react_forms/src/form/index.tsx
@@ -5,10 +5,29 @@ import * as React from 'react'
 
 const styles = require('./styles.scss')
 
-class Form extends React.Component<{}, {}>{
+interface State{
+    name: string
+}
 
+class Form extends React.Component<{}, State>{
+
+    public constructor(props: {}){
+        super(props)
+        this.state = {
+            name: ''
+        }
+    }
+    
     private handleFormSubmit = (event: React.SyntheticEvent<any>) => {
         console.log('Form was submitted')
+        console.log('New name is: ' + this.state.name)
+    }
+    
+    private handleNameChanged = (event: React.FormEvent<any>) => {
+        const newName = (event.target as any).value
+        this.setState({
+            name: newName
+        })
     }
 
     public render(){
@@ -16,7 +35,10 @@ class Form extends React.Component<{}, {}>{
             <div className={styles.container}>
                 <label>
                     Name:
-                    <input type="text" name="name" />
+                    <input type="text"
+                           name="name" 
+                           value={this.state.name}
+                           onChange={this.handleNameChanged} />
                 </label>
                 <button onClick={this.handleFormSubmit}>Save</button>
             </div>
