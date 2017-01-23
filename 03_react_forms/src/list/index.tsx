6db4b47828dc0f79711e05e6ac010d668ab9d396
Antoine Benain <antoine.benain@gmail.com>
2017-01-23 23:03:45 +0100

    Chapter 03: add document button

--- 03_react_forms/src/list/index.tsx
+++ 03_react_forms/src/list/index.tsx
@@ -2,11 +2,12 @@
  * Created by antoine on 23/01/2017.
  */
 import * as React from 'react'
-import {Paper, List as MaterialUIList, ListItem} from 'material-ui'
+import {Paper, Divider, FloatingActionButton, List as MaterialUIList, ListItem} from 'material-ui'
 
 import { Document } from '../types'
 
 const styles = require('./styles.scss')
+const plusIcon = require('./plus.png')
 
 interface Props {
     documents: Document[],
@@ -15,13 +16,23 @@ interface Props {
 
 const List = (props: Props) => (
     <Paper className={styles.card}>
-        <MaterialUIList>
-            {props.documents.map(document => (
-                <ListItem key={document.reference}
-                          primaryText={document.title}
-                          onClick={() => props.onDocumentSelected(document.reference)}/>
-            ))}
-        </MaterialUIList>
+        <div className={styles.cardContents}>
+            <div className={styles.listPanel}>
+                <MaterialUIList>
+                    {props.documents.map(document => (
+                        <ListItem key={document.reference}
+                                  primaryText={document.title}
+                                  onClick={() => props.onDocumentSelected(document.reference)}/>
+                    ))}
+                </MaterialUIList>
+            </div>
+            <div className={styles.bottomPanel}>
+                <Divider />
+                <FloatingActionButton className={styles.actionButton} mini={true} onClick={() => {alert('clicked')}} >
+                    <img src={plusIcon} alt="+"/>
+                </FloatingActionButton>
+            </div>
+        </div>
     </Paper>
 )
 
