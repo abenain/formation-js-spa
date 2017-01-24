bd31b59ec48def9330aa43359ad8d9a3ebb52246
Antoine Benain <antoine.benain@gmail.com>
2017-01-24 02:20:55 +0100

    Chapter 03: highlight selected element

--- 03_react_forms/src/root/index.tsx
+++ 03_react_forms/src/root/index.tsx
@@ -5,7 +5,7 @@ import * as ReactDOM from 'react-dom'
 import * as React from 'react'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import Application from 'application'
+import Application from '../application'
 
 require('./styles.scss')
 
